305400990b5a2a9ba2e289d4a1c334eafb1eec8d
Onkar Bharatesh <onkarbk1@gmail.com>
2021-09-18 20:06:44 +0530

    final commit

---
+++ src/components/FilteringTable.js
@@ -0,0 +1,58 @@
+import React, { useMemo } from 'react'
+import { useTable, useFilters } from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import { COLUMNS } from './columns'
+import './table.css'
+
+
+export const FilteringTable = () => {
+
+    const columns = useMemo(() => COLUMNS, [])
+    const data = useMemo(() => MOCK_DATA, [])
+
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+        columns,
+        data
+    }, useFilters)
+
+
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {
+                    headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {
+                                headerGroup.headers.map(column => (
+                                    <th {...column.getHeaderProps()}>{column.render('Header')}
+                                        <div>{column.canFilter ? column.render('Filter') : null}</div>
+
+                                    </th>
+                                ))
+                            }
+                        </tr>
+                    ))
+                }
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {
+                    rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map(cell => {
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                    })
+                                }
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    )
+}
+
+// export default FilteringTable
---
+++ src/components/columns.js
@@ -0,0 +1,29 @@
+import { ColumnFilter } from './ColumnFilter'
+
+export const COLUMNS = [
+    {
+        Header: 'Id', 
+        accessor: 'id', 
+        Filter: ColumnFilter
+    }, 
+    {
+        Header: 'Name',
+        accessor: 'name', 
+        Filter: ColumnFilter
+    },
+    {
+        Header: 'Class',
+        accessor: 'class', 
+        Filter: ColumnFilter
+    },
+    {
+        Header: 'Gender',
+        accessor: 'gender', 
+        Filter: ColumnFilter
+    }, 
+    {
+        Header: 'Marks',
+        accessor: 'marks', 
+        Filter: ColumnFilter
+    }
+]
